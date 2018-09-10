3dbcfde78ef520d872dfaec3a8e2eca144778085
Ihar Papou <igarok-91@mail.ru>
2018-09-10 23:52:48 +0300

    love-triangle

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count = 0;
+	for (i = 0; i < preferences.length; i++) {
+		var x = preferences[i];
+		var y = preferences[x - 1];
+		var z = preferences[y - 1];
+		if (z == i + 1 && x != y) {
+			count ++;
+		}
+	}
+	return Math.floor(count / 3);
 };
